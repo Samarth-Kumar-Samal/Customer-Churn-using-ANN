8679b591f8c9ea4a110071668d22f980bb9309c6
SAMARTH KUMAR SAMAL <samsamal1606@gmail.com>
2023-12-29 17:21:53 +0530

    code changes for app.py and main.yml

--- app.py
+++ app.py
@@ -7,38 +7,21 @@ import tensorflow as tf
 model = tf.keras.models.load_model('ann_model.h5')
 scaler = pickle.load(open('Scaler.pkl', 'rb'))
 
-def predict_api(data):
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    pred = model.predict(new_data)
-    output = np.where(pred < 0.5,0,1)
-    return output[0][0]
-
 def predict(data):
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     pred = model.predict(final_input)[0]
-    output = np.where(pred < 0.5,0,1)
+    output = np.where(pred < 0.5, 0, 1)
     return output[0]
 
 def main():
     st.title('Customer Churn Prediction')
 
-    # Sidebar
-    st.sidebar.header('Input Data')
-    input_data = {}
-    headings = ["Gender", "SeniorCitizen", "Partner", "Dependents", "Tenure", "PhoneService", "MultipleLines", "InternetService", "OnlineSecurity", "OnlineBackup", "DeviceProtection", "TechSupport", 
-                "StreamingTV", "StreamingMovies", "Contract", "PaperlessBilling", "PaymentMethod", "MonthlyCharges", "TotalCharges"]
-    for feature in headings:
-        # Add a unique key for each st.number_input
-        input_data[feature] = st.sidebar.number_input(f'Enter {feature}', value=0.0, key=f'{feature}_sidebar')
-
-    # Predict using API
-    if st.sidebar.button('Predict'):
-        prediction = predict_api(input_data)
-        st.sidebar.success(f'The Customer Churn prediction is {prediction}')
-
-    # Predict using form
+    # Form to take input
     st.header('Predict using Form')
     form_data = {}
+    headings = ["Gender", "SeniorCitizen", "Partner", "Dependents", "Tenure", "PhoneService", "MultipleLines", "InternetService", "OnlineSecurity", "OnlineBackup", "DeviceProtection", "TechSupport", 
+                "StreamingTV", "StreamingMovies", "Contract", "PaperlessBilling", "PaymentMethod", "MonthlyCharges", "TotalCharges"]
+    
     for feature in headings:
         # Add a unique key for each st.number_input
         form_data[feature] = st.number_input(f'Enter {feature}', value=0.0, key=f'{feature}_form')
